c2d52cea5bf21935d1a77a91c4fa7e2c1c192100
OdiDeveloper <alur1001@mail.ru>
2023-08-20 11:47:03 +0300

    6

---
+++ Lesson6.2/Lesson6.2.py
@@ -0,0 +1,13 @@
+﻿# Вводится натуральное число X. Подсчитайте количество натуральных делителей числа X (включая 1 и само число). x ≤ 2e9 (2 миллиарда)
+
+x = int(input("enter natural number: "))
+a = 1
+
+if x < 0:
+    print ("Error")
+else:
+    for i in range(a, int(x/2)+1): 
+        if x % i == 0:
+            a += 1
+    print (a)
+            
